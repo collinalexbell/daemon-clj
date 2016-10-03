62d767622311f9ff9787bfd991cd6cde166b1279
slightlycyborg <slightlycyborg@gmail.com>
2016-10-03 02:32:00 +0000

    pushup counting works!!!
    Former-commit-id: 9035131152c718a2096c4328806cf73e38e735db [formerly c4cee95ea5e7ed3131215feb682e22d3db2af1ac]
    Former-commit-id: 46d111f7d1ac5702126724c21c1c4eef45842976
    Former-commit-id: c4cca416f907db8cdab2c22e359f740eb52535cb [formerly 2ed3775fb47c752b057ab1957da19aa87fd18780]
    Former-commit-id: 7c86b59a9b32b8b68ed1fc6128cc9d12b06f7216

--- src/dameon/visual_cortex/pushup_counter.clj
+++ src/dameon/visual_cortex/pushup_counter.clj
@@ -11,6 +11,9 @@
 (def plot (time-series-plot [] []))
 (view plot)
 
+def pushup-plot (time-series-plot [] []))
+(view pushup-plot)
+
 (def last-frames (atom []))
 (def last-frame (atom nil))
 (def frame-history-size 3)
@@ -27,11 +30,41 @@
        (catch Exception e (def except (.getMessage e))))
   (swap! last-frames (fn [lf] (into [] (cons frame lf)))))
 
+(def last-centroid (atom nil))
+(def last-dydx (atom nil))
+(def centroid-extream (atom nil))
+(def movement-threshold 120)
+
+(defn get-centroid-dydx [new-centroid]
+  (if @last-centroid
+    (- (new-centroid :y) (@last-centroid :y))
+    nil))
+
 (defn get-centroid [mat]
   (let [moments (Imgproc/moments mat)]
     {:x (/ (.get_m10 moments) (.get_m00 moments))
      :y (/ (.get_m01 moments) (.get_m00 moments))}))
 
+(defn experienced-v-sign-flip? [new-centroid new-dydx]
+  (if (and
+       ;;centroids and derivatives already must exist
+       @last-centroid @last-dydx
+       ;;test diffent signs (aka passed through 0 derivitave)
+       (<= (* @last-dydx new-dydx) 0.1))
+    true
+    false))
+
+(defn experienced-pushup? [new-centroid new-dydx]
+  (if (and
+       @centroid-extream
+       (experienced-v-sign-flip? new-centroid new-dydx)
+       (< @last-dydx 0)
+       (> (abs (- @centroid-extream (new-centroid :y))) movement-threshold))
+    true
+    false))
+
+(defn handle-pushup-experience []
+  (add-points pushup-plot [(System/currentTimeMillis)] [1]))
 
 (defn get-motion [new-frame]
   (swap! last-frame (constantly (smart-atom/deref new-frame)))
@@ -40,7 +73,8 @@
       (.apply fgbg (smart-atom/deref new-frame) rv)
       (catch Exception e (do (println "exception")
                              (swap! test-exception (constantly (.getMessage e))))))
-    (let [centroid (get-centroid rv)]
+    (let [centroid (get-centroid rv)
+          dydx     (get-centroid-dydx centroid)]
       (try
         (Imgproc/rectangle rv (Point. 20 20) (Point. 120 60) (Scalar. 255.0) -1)
         (Imgproc/putText rv (str "y: "
@@ -49,6 +83,14 @@
                          Core/FONT_HERSHEY_PLAIN 1 (Scalar. 0.0) 2)
 
         (add-points plot [(System/currentTimeMillis)] (centroid :y))
+        (if (experienced-pushup? centroid dydx)
+          (handle-pushup-experience))
+        (if (experienced-v-sign-flip? centroid dydx)
+          ;;we need to make sure that the extreams are greater than a threshold
+          ;;this prevents small dydx sign flips from causing problems
+          (swap! centroid-extream (constantly (centroid :y))))
+        (swap! last-centroid (constantly centroid))
+        (swap! last-dydx (constantly dydx))
         (catch Exception e (println (.getMessage e))))
       {:smart-mat (smart-atom/create rv)})))
 

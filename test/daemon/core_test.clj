5fd7e69795b647ccbd0d1481f134219341739d9f
slightlycyborg <slightlycyborg@gmail.com>
2016-10-10 21:00:39 +0000

    meditation test added
    Former-commit-id: cbc1cb6602898724dc10aa592b28f57de78401f4 [formerly 7d0e8ee3da2043b8b5e6d26da940c2610d84e34a]
    Former-commit-id: 317db7b6cb213c9f34bb98f97a4fd163b74ae740
    Former-commit-id: 15c677d1c590902dd01c29a679fcb4d37eeb69ca [formerly 9de8b403b0b0701a8587fce7bd1a0af5c098d695]
    Former-commit-id: ef68c0842a39c016a595496ab95ff3373deda1e2

--- test/daemon/core_test.clj
+++ test/daemon/core_test.clj
@@ -1,7 +1,9 @@
 (ns daemon.core-test
   (:require [clojure.test :refer :all]
             [daemon.core :refer :all]
-            [daemon.brochas-area.core :as brochas-area]))
+            [daemon.brochas-area.core :as brochas-area]
+            [daemon.voice.core :as voice]
+            [daemon.temporal-lobe.core :as temporal-lobe]))
 
 
 ;;Features to test
@@ -13,7 +15,14 @@
      @(brochas-area/interpret-speech (brochas-area/slurp-bytes "resources/test/test-interpret-speech.wav"))))))
 
 
+(deftest meditation []
+  (voice/speak "Testing meditations. You should hear 2 test meditations with 10 seconds in between.")
+  (Thread/sleep 5000)
+  (temporal-lobe/meditate "30" ["the beauty of clojure" "the beauty of nature"]))
+
 (interpret-speech)
+
+(meditation)
 
 
 

a8fa8af5b11170af545ef6d2cdf714369409de95
slightlycyborg <slightlycyborg@gmail.com>
2016-10-10 20:48:18 +0000

    test added
    Former-commit-id: bb2990324c2264f0c04c5afca97ada1483a5773b [formerly c5655c51ab6a96f5139e44c0fbb789b650b0e123]
    Former-commit-id: 9ffcaea177563a12b7ad7653778b531e64a074d9
    Former-commit-id: b2a9332bcafd65e4d0a53fb2a199f90fd499691d [formerly 05fb4073b087c4f2920a341a4a7152a2037f95e8]
    Former-commit-id: 53fc7cfc299c4730551befd391f2cd6c35bdba1c

--- test/daemon/core_test.clj
+++ test/daemon/core_test.clj
@@ -1,7 +1,33 @@
 (ns daemon.core-test
   (:require [clojure.test :refer :all]
-            [dameon.core :refer :all]))
+            [daemon.core :refer :all]
+            [daemon.brochas-area.core :as brochas-area]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+;;Features to test
+;;;Voice recognition
+(deftest interpret-speech []
+  (is
+   (= "This is a test."
+    (brochas-area/get-words-from-api-result
+     @(brochas-area/interpret-speech (brochas-area/slurp-bytes "resources/test/test-interpret-speech.wav"))))))
+
+
+(interpret-speech)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

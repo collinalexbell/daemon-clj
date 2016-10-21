4554c669713c59fcdd8f27a8a15f7fa243a4dea4
slightlycyborg <slightlycyborg@gmail.com>
2016-10-21 19:16:11 +0000

    added wave analyzer
    Former-commit-id: 86ade79363a8b362d33febfd9a77b75b888441a2 [formerly 10cb07e6b3fc5fbcc1312dcf2ef0a48ccb44e785]
    Former-commit-id: 1a7468e25aec0a2983b61f052d3087a7dd215438
    Former-commit-id: 7aa4733732c551b50086377f0fcc2aac588d6643 [formerly e5244f17da7ff6e75e05d5c282b959380feb1f36]
    Former-commit-id: aad81df5ab474aa32f45662b0eb0f116f40a388e

---
+++ src/daemon/brochas_area/analyzer.clj
@@ -0,0 +1,31 @@
+(ns daemon.brochas-area.analyzer
+  (:use [incanter core stats charts io]))
+
+
+(defn slurp-bytes
+  "Slurp the bytes from a slurpable thing"
+  [x]
+  (with-open [out (java.io.ByteArrayOutputStream.)]
+    (clojure.java.io/copy (clojure.java.io/input-stream x) out)
+    (.toByteArray out)))
+
+
+
+(def demo-recording (slurp-bytes "resources/demo.wav"))
+
+
+(def plot (xy-plot
+           (range)
+           (subvec
+            (into [] (take 650000
+                            (map
+                             #(- (bit-and (int %) 0xff) 0x7f)
+                            demo-recording)))
+             50000 55000)))
+
+(count demo-recording)
+
+(set-point-size plot 1)
+(view plot)
+
+
